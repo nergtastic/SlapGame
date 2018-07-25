653000aea68b1e6a77090aa857398063ad6230e7
nergtastic <nergon@gmail.com>
2018-07-24 20:52:26 -0600

    Slappee gets knocked down now, defenses automatically run, added restart .

--- game.js
+++ game.js
@@ -38,14 +38,14 @@ function giveShield(){
 function blockFace(){
     boxer.items.push(items.blockFace)
     faceBlock = true
-    faceBlockCount = 5
+    faceBlockCount = 10
     update()
 }
 
 function blockBody(){
     boxer.items.push(items.blockBody)
     bodyBlock = true
-    bodyBlockCount = 10
+    bodyBlockCount = 20
     update()
 }
 
@@ -137,7 +137,7 @@ function kick(){
     console.log(mod)
     if(boxer.health != 0){
         boxer.health -= 10/ (1 + mod)
-        if(boxer.health < 0 || (boxer.health > 0 && boxer.health < 1)){
+        if(boxer.health < 0 || (boxer.health > 0 && boxer.health < 1)){ //If the math gives a negative health or that of less than 1, then set health to 0.
             boxer.health = 0
         }
         boxer.hits++
@@ -157,36 +157,22 @@ function update(){
         randomPicture = (Math.floor(Math.random() * 3)) + 1
     } while (randomPicture == previousPicture)
     document.getElementById('slappee').src = './img/Boxer' + randomPicture + '.png'
-    // if(mouthGuardCount > 0 || faceBlockCount > 0 || bodyBlockCount > 0){  //if the active item timer is greater than 0 then decrement the counter, otherwise clear active items.
-    //     activeCount--
-    //     console.log(activeCount)
-    // } else if (activeCount == 0){
-    //     if (mouthGuard){
-    //         takeShield()
-    //     } else if (faceBlock) {
-    //         unblockFace()
-    //     } else if (bodyBlock) {
-    //         unblockBody()
-    //     }
 
-    //     console.log('Active item timed out.')
-    // }
-
-    if(mouthGuardCount > 0){
+    if(mouthGuardCount > 0){ //if the target has the mouth guard, then continue counting down
         mouthGuardCount--
         console.log(mouthGuardCount) 
     } else if (mouthGuardCount == 0 && mouthGuard){
         takeShield()
     }
 
-    if(faceBlockCount > 0){
+    if(faceBlockCount > 0){ //if the target is blocking his face, then continue counting down
         faceBlockCount--
         console.log(faceBlockCount)
     } else if (faceBlockCount == 0 && faceBlock){
         unblockFace()
     }
 
-    if(bodyBlockCount > 0){
+    if(bodyBlockCount > 0){ //if the target is blocking his body, then continue counting down
         bodyBlockCount--
         console.log(bodyBlockCount)
     } else if (bodyBlockCount == 0 && bodyBlock){
@@ -216,14 +202,66 @@ function update(){
 
 }
 
+function startOver(){
+    boxer.health = 100
+
+    mouthGuard = false
+    faceBlock = false
+    bodyBlock = false
+
+    mouthGuardCount = 0
+    faceBlockCount = 0
+    bodyBlockCount = 0
+
+    boxer.hits = 0
+
+    document.getElementById("restart").style.display = 'none'
+    document.getElementById("slappee").style.marginTop = '-160px'
+    document.getElementById("slappee").style.width = '53%'
+    document.getElementById("healthBar").style.width = '100%'
+    document.getElementById("health-tally").innerText = '100'
+    document.getElementById("hits-tally").innerText = '0'
+
+    boxer.items = []
+
+    document.getElementById("mouthGuard").disabled = false
+    document.getElementById("blockFace").disabled = false
+    document.getElementById("blockBody").disabled = false
+
+    document.getElementById("slap").disabled = false
+    document.getElementById("punch").disabled = false
+    document.getElementById("kick").disabled = false
+
+    update()
+}
+
 document.getElementById("healthBar").width = Math.round(boxer.health.toString()) + '%'
+
 
 let animateBoxer = setInterval(function(){
     if(boxer.health > 0){
+        let randomDefense = (Math.floor(Math.random() * 25)) + 1
+        console.log(randomDefense)
+        if (randomDefense == 12 && !faceBlock){
+            blockFace()
+        } else if (randomDefense == 7 && !bodyBlock) {
+            blockBody()
+        } 
         update()
+    } else {
+        document.getElementById("restart").style.display = 'block'
+        // document.getElementById("slappee").style.display = 'none'
+        document.getElementById("mouthGuard").disabled = true
+        document.getElementById("blockFace").disabled = true
+        document.getElementById("blockBody").disabled = true
+        document.getElementById("slap").disabled = true
+        document.getElementById("punch").disabled = true
+        document.getElementById("kick").disabled = true
+        document.getElementById('slappee').src = './img/BoxerDown.png'
+        document.getElementById('slappee').style.marginTop = '50px'
+        document.getElementById('slappee').style.width = '70%'
     }
-}, 200);
-
+}, 175);
 
 
 
